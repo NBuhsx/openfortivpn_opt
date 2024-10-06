73df271f7b047abf19a5d620a6b46199bf85c0ac
NBuhsx <trubitsyn98@gmail.com>
2024-10-06 17:15:26 +0300

    first

---
+++ main.py
@@ -0,0 +1,25 @@
+import json
+import pyotp
+import subprocess
+
+
+def main():
+    with open(file="config.json", mode="r") as file:
+        config = json.load(file)
+
+    code = pyotp.parse_uri(config["opt"])
+    print("OTP: {code}")
+    subprocess.run(
+        [
+            "sudo",
+            "openfortivpn",
+            f"{config['host']}:{config['port']}",
+            f"--username={config['user']}",
+            f"--password={config['password'] + code.now()}",
+            f"--trusted-cert={config['trusted-cert']}"
+        ])
+    exit()
+
+
+if __name__ == "__main__":
+    main()
---
+++ readme.md
@@ -0,0 +1,22 @@
+# Openforti pass + opt
+
+## Install 
+* 
+        https://github.com/adrienverge/openfortivpn
+*
+        ~ pip install - r requirements.txt
+## Create
+    ~ touch config.json
+
+```json
+{
+    "host": "",
+    "port": "",
+    "user": "",
+    "password": "",
+    "opt": "otpauth://",
+    "trusted-cert": ""
+}
+```
+## Run
+    ~ python main.py
